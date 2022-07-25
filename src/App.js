e10d1c60b4aaf594c50f021169a06a2fe1cc35f1
izoutendijk <101944772+izoutendijk@users.noreply.github.com>
2022-07-25 21:02:27 +0200

    Added responsive search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Is everything working?</h1>
-      <img src={logo} alt="logo" className="App-logo img-fluid"></img>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} alt="logo" className="App-logo img-fluid"></img>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer text-center">
+          Open Source Code by Iris Zoutendijk
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Loading the definition of ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          autoFocus={true}
+        ></input>
+      </form>
+    </div>
+  );
+}
