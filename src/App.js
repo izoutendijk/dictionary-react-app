3071c40eee3fada6a60617916aa8bd735303506e
izoutendijk <101944772+izoutendijk@users.noreply.github.com>
2022-07-25 17:16:49 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Is everything working?</h1>
+      <img src={logo} alt="logo" className="App-logo img-fluid"></img>
+    </div>
+  );
+}
+
+export default App;
