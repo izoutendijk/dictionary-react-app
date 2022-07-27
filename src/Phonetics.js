2761de18dd3242a6d6c00848bb3ddbde8798adea
izoutendijk <101944772+izoutendijk@users.noreply.github.com>
2022-07-27 19:04:15 +0200

    Addition of Audio player and some css

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,14 +1,18 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
   return (
     <div className="Phonetics">
-      <a href={props.data.audio} target="blank">
-        Listen
-      </a>
       <div className="phoneticText">{props.data.text}</div>
+      <ReactAudioPlayer
+        id="audioplayer"
+        src={props.data.audio}
+        autoPlay={false}
+        controls
+      />
     </div>
   );
 }
